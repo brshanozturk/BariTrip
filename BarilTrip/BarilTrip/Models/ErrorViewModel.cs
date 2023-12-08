cc72786c9eb78088f5b22987ee4df79775fc5b0e
Barışhan Öztürk <149052051+brshanozturk@users.noreply.github.com>
2023-12-08 03:30:14 +0300

    FirstTime

---
+++ BarilTrip/BarilTrip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarilTrip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

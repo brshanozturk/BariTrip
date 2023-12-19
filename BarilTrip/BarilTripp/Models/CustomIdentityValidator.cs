6380c21339101e4c368f9b42d3e308ddc1330006
Barışhan Öztürk <149052051+brshanozturk@users.noreply.github.com>
2023-12-19 18:25:40 +0300

    Güncelleme

---
+++ BarilTrip/BarilTripp/Models/CustomIdentityValidator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BarilTripp.Models
+{
+	public class CustomIdentityValidator:IdentityErrorDescriber
+	{
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code = "PasswordTooShort",
+				Description = $"Parola Minumum {length} karakter olmalıdır"
+
+			};
+		}
+	}
+}

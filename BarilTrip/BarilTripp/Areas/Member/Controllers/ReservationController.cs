947aa2e738ba2fdc1fedc90d3ca58a10c98ea8a1
Barışhan Öztürk <149052051+brshanozturk@users.noreply.github.com>
2023-12-21 02:35:37 +0300

    Güncelleme

--- BarilTrip/BarilTripp/Areas/Member/Controllers/ReservationController.cs
+++ BarilTrip/BarilTripp/Areas/Member/Controllers/ReservationController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BarilTripp.Areas.Member.Controllers
 {
@@ -13,6 +15,13 @@ namespace BarilTripp.Areas.Member.Controllers
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
+        private readonly UserManager<AppUser> _userManager;
+
+        public ReservationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult MyCurrentReservation()
         {
 
@@ -21,6 +30,12 @@ namespace BarilTripp.Areas.Member.Controllers
         public IActionResult MyOldReservation()
         {
             return View();
+        }
+        public async Task <IActionResult> MyApprovalReservation()
+        {
+            var values=await _userManager.FindByNameAsync(User.Identity.Name);
+            var valuesList = reservationManager.GetListApprovalReservation(values.Id);
+            return View(valuesList);
         }
         [HttpGet]
         public IActionResult NewReservation() 

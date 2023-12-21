b1ae21e3027b179966b4d2acb9149f30ee992107
Barışhan Öztürk <149052051+brshanozturk@users.noreply.github.com>
2023-12-21 23:19:47 +0300

    Güncelleme

---
+++ BarilTrip/BarilTripp/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarilTripp.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+
+    }
+}
